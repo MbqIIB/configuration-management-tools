9424b5d429ce9fc4ff5fab84f52e3748eab7de29
Santosh Spring <santosh.spring32@gmail.com>
2017-06-02 12:35:16 +0530

    linux flavors added

---
+++ devops/chef/chef-starter/chef-repo/cookbooks/tomcat_on_linuxflavors/recipes/tomcat.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook:: .
+# Recipe:: tomcat
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+package_name = node['tomcat_on_linuxflavors']['package_name']
+
+package package_name do
+  action :install
+end
+
+service package_name do
+  action :start
+end
