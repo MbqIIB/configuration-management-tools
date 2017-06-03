96c15a8cd6bca9ccd930c08352d93fd6467b5b44
Santosh Spring <santosh.spring32@gmail.com>
2017-06-03 15:26:32 +0530

    tomcat_on_ec2: jenkins remore_file location added

--- devops/chef/chef-starter/chef-repo/cookbooks/tomcat_on_ec2/attributes/default.rb
+++ devops/chef/chef-starter/chef-repo/cookbooks/tomcat_on_ec2/attributes/default.rb
@@ -3,7 +3,7 @@ case node['platform']
     default['tomcat_on_ec2']['package_name'] = 'tomcat7'
     default['tomcat_on_ec2']['additional_packages'] = ['tomcat7-docs','tomcat7-admin','tomcat7-examples']
     default['webserver']['users'] = '/etc/tomcat7/tomcat-users.xml'
-    default['tomcat_on_ec2']['appdeploylocation'] = '/var/lib/tomcat7/webapps/jenkins.war'
+    default['tomcat_on_ec2']['deploy_location'] = '/var/lib/tomcat7/webapps/jenkins.war'
     default['tomcat_on_ec2']['tomcatconfigfile'] = '/etc/default/tomcat7'
     default['tomcat_on_ec2']['templatename'] = 'tomcat7.erb'
   when 'rhel', 'centos'
@@ -11,9 +11,11 @@ case node['platform']
     default['tomcat_on_ec2']['tomcatconfigfile'] = '/usr/share/tomcat/conf/tomcat.conf'
     default['tomcat_on_ec2']['templatename'] = 'tomcat.conf.erb'
     default['tomcat_on_ec2']['additional_packages'] = ['tomcat-webapps','tomcat-admin-webapps','tomcat-docs-webapp','tomcat-javadoc']
-    default['tomcat_on_ec2']['appdeploylocation'] = '/usr/share/tomcat/webapps/jenkins.war'
+    default['tomcat_on_ec2']['deploy_location'] = '/usr/share/tomcat/webapps/jenkins.war'
 end
 
 default['webserver']['username'] = 'admin'
 default['webserver']['password'] = 'password'
 default['webserver']['roles'] = 'manager-gui,admin-gui'
+
+default['tomcat_on_ec2']['jenkins'] = "http://ftp.yz.yamagata-u.ac.jp/pub/misc/jenkins/war-stable/2.46.2/jenkins.war"
