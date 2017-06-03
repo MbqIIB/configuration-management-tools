ecd7c23437ff1b6996783ebce7f7e0548cbf2112
Santosh Spring <santosh.spring32@gmail.com>
2017-06-03 15:36:31 +0530

    tomcat_on_ec2: jenkins remore_file location added

--- devops/chef/chef-starter/chef-repo/cookbooks/tomcat_on_ec2/recipes/deploy_archives.rb
+++ devops/chef/chef-starter/chef-repo/cookbooks/tomcat_on_ec2/recipes/deploy_archives.rb
@@ -3,8 +3,9 @@
 # Recipe:: deploy_archives
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-remote_file node['tomcat_on_ec2']['deploy_location'] do
-  source node['tomcat_on_ec2']['jenkins']
-  action :create
-  notifies :restart, "service[#{node['tomcat_on_ec2']['package_name']}]"
-end
+
+#remote_file node['tomcat_on_ec2']['deploy_location'] do
+ # source node['tomcat_on_ec2']['jenkins']
+  #action :create
+  #notifies :restart, "service[#{node['tomcat_on_ec2']['package_name']}]"
+#end
