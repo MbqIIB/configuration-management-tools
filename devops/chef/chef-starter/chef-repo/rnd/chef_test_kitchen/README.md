66672d298801af65102392a26405b3ffdfe58ecc
Spring32 <spring4buddies@gmail.com>
2017-06-08 19:30:27 +0530

    chef_test_kitchen added
    Change-Id: Ia51f0a7511d410a0590d04a043eb036f77df8e25

---
+++ devops/chef/chef-starter/chef-repo/rnd/chef_test_kitchen/README.md
@@ -0,0 +1,11 @@
+$ kitchen init --create-gemfile
+
+$ bundle install
+
+$ kitchen list
+
+$ kitchen create default-centos65
+
+$ kitchen login default-centos65
+
+$ kitchen destroy default-centos65
---
+++ devops/chef/chef-starter/chef-repo/rnd/chef_test_kitchen/metadata.rb
@@ -0,0 +1,8 @@
+name 'chef_test_kitchen'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures chef_test_kitchen'
+long_description 'Installs/Configures chef_test_kitchen'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
