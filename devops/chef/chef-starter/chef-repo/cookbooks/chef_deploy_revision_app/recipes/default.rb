d46b99b1075fc91ea0ac60ef5cbcb227d50a560d
Santosh Spring <santosh.spring32@gmail.com>
2017-06-14 23:19:44 +0530

    Change-Id: I351c17797e99034c8ccc4d8229828bca9d72936f

--- devops/chef/chef-starter/chef-repo/cookbooks/chef_deploy_revision_app/recipes/default.rb
+++ devops/chef/chef-starter/chef-repo/cookbooks/chef_deploy_revision_app/recipes/default.rb
@@ -4,7 +4,6 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-# Clone and sync an application from revision control
 deploy_revision '/opt/my_app' do
-  repo 'git://github.com/username/app.git'
+  repo 'git@github.com:santosh32/spring-app-with-jenkins.git'
 end
