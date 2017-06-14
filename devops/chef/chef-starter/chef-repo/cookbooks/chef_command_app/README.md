adea86d7ba3553702e99fd2b954818170a0c8b44
Santosh Spring <santosh.spring32@gmail.com>
2017-06-14 22:33:40 +0530

    Change-Id: I1e378eaf9311a1ecabe72ec32446e9a459719131

---
+++ devops/chef/chef-starter/chef-repo/cookbooks/chef_command_app/README.md
@@ -0,0 +1,25 @@
+# chef_command_app
+
+TODO: Enter the cookbook description here.
+
+Create Vagrant Machine for Node
+-
+- vagrant init ubuntu/trusty64
+- vagrant up
+- vagrant ssh
+vagrant@localhost > sudo -i
+root@localhost > 
+
+# run following command where ever the .chef folder is present
+chef-repo> knife bootstrap 192.168.33.10 -x vagrant -P vagrant --sudo -N ubuntu.vagrant.com
+
+chef_command_app> knife cookbook upload chef_command_app
+
+From Hosted Server -
+-
+1. select 'run_list' from node
+2. add recipe like chef_command_app
+3. save the run list
+4. run the 'chef-client' from terminal
+	root@localhost > chef-client
+
---
+++ devops/chef/chef-starter/chef-repo/cookbooks/chef_command_app/recipes/default.rb
@@ -0,0 +1,49 @@
+#
+# Cookbook:: chef_command_app
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+# 'directory' Recursively ensure a directory exists
+directory '/opt/my/deep/directory' do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  recursive true
+end
+
+# 'execute' Write contents to a file
+execute 'write status' do
+  command 'echo "delicious" > /tmp/bacon'
+end
+
+# 'file' Delete the /tmp/bacon file
+#file '/tmp/bacon' do
+ # action :delete
+#end
+
+# 'gem_package' Install bundler to manage dependencies
+# gem_package 'bundler'
+
+# 'group' Create the bacon group
+# group 'bacon'
+
+# Create the bacon user
+# user 'bacon'
+
+# Link /tmp/bacon to /tmp/delicious
+#link '/tmp/bacon' do
+ # to '/tmp/delicious'
+#end
+
+# Mount /dev/sda8
+# mount '/dev/sda8'
+
+# Install the apache2 package (on Debian-based systems)
+# package 'apache2'
+
+# 'remote_file' Download a remote file to /tmp/bacon
+#remote_file '/tmp/bacon' do
+ # source 'http://bacon.org/bits.tar.gz'
+#end
+
