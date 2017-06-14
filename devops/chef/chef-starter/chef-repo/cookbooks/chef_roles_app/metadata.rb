3a06b3846e80145b3caed9208f1d11c61638fbe1
Santosh Spring <santosh.spring32@gmail.com>
2017-06-15 01:33:25 +0530

    Change-Id: I8c0e8714f7878801ccc92f6cd5de989b2d2e1ebb

--- devops/chef/chef-starter/chef-repo/cookbooks/chef_roles_app/metadata.rb
+++ devops/chef/chef-starter/chef-repo/cookbooks/chef_roles_app/metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures chef_roles_app'
 long_description 'Installs/Configures chef_roles_app'
-version '0.1.1'
+version '0.1.5'
 chef_version '>= 12.1' if respond_to?(:chef_version)
