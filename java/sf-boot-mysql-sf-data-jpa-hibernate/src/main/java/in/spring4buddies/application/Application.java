e03c42b6489b31cdfaf585d4338dedf97bacaa4a
Santosh Spring <santosh.spring32@gmail.com>
2017-08-13 00:28:50 +0530

    Change-Id: I3fea4710d3edc6a77ca6031c6651011d8fcb7255

--- java/sf-boot-mysql-sf-data-jpa-hibernate/src/main/java/in/spring4buddies/application/Application.java
+++ java/sf-boot-mysql-sf-data-jpa-hibernate/src/main/java/in/spring4buddies/application/Application.java
@@ -7,6 +7,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		 SpringApplication.run(Application.class, args);
+
+		/*Connection conn = null;
+
+		try {
+			String userName = "root";
+			String password = "root";
+
+			String url = "jdbc:mysql://54.202.11.121:3306/mydb";
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			conn = DriverManager.getConnection(url, userName, password);
+			System.out.println("Database connection established");
+		} catch (Exception e) {
+			System.err.println("Cannot connect to database server");
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+					System.out.println("Database Connection Terminated");
+				} catch (Exception e) {
+				}
+			}
+		}*/
 	}
 }
